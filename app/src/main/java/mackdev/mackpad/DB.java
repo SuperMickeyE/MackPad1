f0efb53f9fd895d20c8af5f134da462077220cc0
Micah Dumont <micahedumont@gmail.com>
2016-12-01 08:57:30 -0600

    Added Alarm Column to Database

--- app/src/main/java/mackdev/mackpad/DB.java
+++ app/src/main/java/mackdev/mackpad/DB.java
@@ -16,6 +16,7 @@ public final class DB {
         public static final String TABLE_NAME = "note";
         public static final String COLUMN_NAME_TEXT = "text";
         public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_ALARM = "alarm";
     }
 
     public static final String TEXT_TYPE = " TEXT";
@@ -24,7 +25,9 @@ public final class DB {
             "CREATE TABLE " + DBNotes.TABLE_NAME + " (" +
                     DBNotes._ID + " INTEGER PRIMARY KEY," +
                     DBNotes.COLUMN_NAME_TEXT + TEXT_TYPE + COMMA_SEP +
-                    DBNotes.COLUMN_NAME_DATE + " DATE" + " DEFAULT NULL )";
+                    DBNotes.COLUMN_NAME_DATE + " DATE" + " DEFAULT NULL " +
+                    DBNotes.COLUMN_NAME_ALARM + " TINYINT" + " DEFAULT 0" +
+                    ")";
     public static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + DBNotes.TABLE_NAME;
 }
